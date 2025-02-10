69a151a27533fe595f809765acba3aaeab0d469c
Chris Thomas <22000088@hope.ac.uk>
2025-02-10 16:15:43 +0000

    fix for vite.config #3

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,12 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
- 
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -9,4 +14,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
